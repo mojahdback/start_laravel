b3e9a79454e63df880502d43e3bb3b4f1831c7d7
mohamed <mojahdontheway@gmail.com>
2026-01-30 08:21:10 +0100

    add Controller for Category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+          $request->validate([
+            'nom' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Category::create([
+            'nom' => $request->nom,
+            'slug' => Str::slug($request->nom),
+            'description' => $request->description,
+        ]);
+
+        return redirect()->route('categories.index')
+                         ->with('success', 'Catégorie créée avec succès');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+         return view('categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nom' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+        $category = Category::findOrFail($id);
+
+        $category->update([
+            'nom' => $request->nom,
+            'slug' => Str::slug($request->nom),
+            'description' => $request->description,
+        ]);
+
+        return redirect()->route('categories.index')
+                         ->with('success', 'Catégorie mise à jour');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+          $category = Category::findOrFail($id);
+         $category->delete();
+
+        return redirect()->route('categories.index')
+                         ->with('success', 'Catégorie supprimée');
+    }
+}
